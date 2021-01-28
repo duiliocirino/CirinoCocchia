cd4bb1fa758230a899a1315ff6b56c4caefdf292
cocchialorenzo9 <lorenzo.cocchia@mail.polimi.it>
2021-01-28 15:59:17 +0100

    Search Engine Module implementation and unit testing

--- SourceCode/BusinessLogicTier/SE2-project-CirinoCocchia-EJB/ejbModule/services/searchManagement/implementation/SearchEngineModuleImplementation.java
+++ SourceCode/BusinessLogicTier/SE2-project-CirinoCocchia-EJB/ejbModule/services/searchManagement/implementation/SearchEngineModuleImplementation.java
@@ -3,6 +3,7 @@ package services.searchManagement.implementation;
 import java.util.ArrayList;
 import java.util.List;
 
+import exceptions.CLupException;
 import model.Grocery;
 import model.Position;
 import model.User;
@@ -11,11 +12,18 @@ import services.searchManagement.interfaces.SearchEngineModule;
 public class SearchEngineModuleImplementation extends SearchEngineModule {
 
 	@Override
-	public List<Grocery> getNearGroceries(Position position, double radius) {
+	public List<Grocery> getNearGroceries(Position position, double radius) throws CLupException {
 		
-		List<Grocery> allGroceries = em.createNamedQuery("Grocery.findAll", Grocery.class)
-				.getResultList();
+		List<Grocery> allGroceries = namedQueryGroceryFindAll();
 		List<Grocery> nearGroceries = new ArrayList<Grocery>();
+		
+		if(position == null) {
+			throw new CLupException("Can't compute a null position");
+		}
+		
+		if(radius < 0) {
+			throw new CLupException("Can't compute a radius less than 0");
+		}
 		
 		for(Grocery grocery : allGroceries) {
 			if(isNear(position, grocery.getIdgrocery(), radius)) {
@@ -27,32 +35,37 @@ public class SearchEngineModuleImplementation extends SearchEngineModule {
 	}
 
 	@Override
-	public List<Grocery> getFavouriteGroceries(int iduser, int nFavourites) {
-		User customer = em.find(User.class, iduser);
+	public List<Grocery> getFavouriteGroceries(int iduser, int nFavourites) throws CLupException {
+		User customer = findUser(iduser);
 		
 		if(customer == null) {
-			return null;
+			throw new CLupException("Can't find the user in the DB");
 		}
 		
 		if(nFavourites < 1) {
-			return null;
+			throw new CLupException("Can't retrieve a number of favourite grociries"
+					+ " less than 0");
 		}
 		
-		List<Grocery> favouriteGroceries = em.createNamedQuery("Reservation.findCustomersFavourites", Grocery.class)
-				.setParameter("customer", customer)
-				.setParameter("nFav", nFavourites)
-				.getResultList();
-		
+		List<Grocery> favouriteGroceries = namedQueryReservationFindCustomersFavourites(customer, nFavourites);	
 				
 		return favouriteGroceries;
 	}
 
 	@Override
-	public boolean isNear(Position position, int idgrocery, double radius) {
+	public boolean isNear(Position position, int idgrocery, double radius) throws CLupException {
 		
-		Grocery grocery = em.find(Grocery.class, idgrocery);
+		Grocery grocery = findGrocery(idgrocery);
 		if(grocery == null) {
-			return false; //TODO : exception
+			throw new CLupException("Can't find the grocery");
+		}
+		
+		if(position == null) {
+			throw new CLupException("Can't compute a null position");
+		}
+		
+		if(radius < 0) {
+			throw new CLupException("Can't compute a radius less than 0");
 		}
 		
 		double maxLat = position.getLat() + radius;
@@ -60,10 +73,30 @@ public class SearchEngineModuleImplementation extends SearchEngineModule {
 		double maxLon = position.getLon() + radius;
 		double minLon = position.getLon() - radius;
 				
-		return (minLat < grocery.getLatitude() && 
-						grocery.getLatitude() < maxLat) &&
-				(minLon < grocery.getLongitude() &&
-						grocery.getLongitude() < maxLon);
+		return (minLat <= grocery.getLatitude() && 
+						grocery.getLatitude() <= maxLat) &&
+				(minLon <= grocery.getLongitude() &&
+						grocery.getLongitude() <= maxLon);
+	}
+	
+	protected User findUser(int iduser) {
+		return em.find(User.class, iduser);
+	}
+	
+	protected Grocery findGrocery(int idgrocery) {
+		return em.find(Grocery.class, idgrocery);
+	}
+	
+	protected List<Grocery> namedQueryGroceryFindAll(){
+		return em.createNamedQuery("Grocery.findAll", Grocery.class)
+				.getResultList();
+	}
+	
+	protected List<Grocery> namedQueryReservationFindCustomersFavourites(User customer, int nFav) {
+		return em.createNamedQuery("Reservation.findCustomersFavourites", Grocery.class)
+				.setParameter("customer", customer)
+				.setParameter("nFav", nFav)
+				.getResultList();
 	}
 
 }
