453668fb4056e3cda033d500181c4a29840bfdb4
duiliocirino <10621933@polimi.it>
2021-01-31 17:24:20 +0100

    Added utility methods in GroceryData.java.

--- SourceCode/BusinessLogicTier/SE2-project-CirinoCocchia-EJB/ejbModule/src/main/java/utils/GroceryData.java
+++ SourceCode/BusinessLogicTier/SE2-project-CirinoCocchia-EJB/ejbModule/src/main/java/utils/GroceryData.java
@@ -1,6 +1,30 @@
 package src.main.java.utils;
 
 public enum GroceryData {
-	NUM_WEEK_CUSTOMERS, NUM_MONTH_CUSTOMERS,
-	AVG_TIME_WEEK, AVG_TIME_MONTH;
+	AVG_WEEK_CUSTOMERS("Average customers per week"), AVG_MONTH_CUSTOMERS("Average customers per month"),
+	AVG_TIME_WEEK("Average time per week"), AVG_TIME_MONTH("Average time per month");
+	
+	private final String value;
+	
+	GroceryData(String string) {
+		value = string;
+	}
+
+	public static GroceryData getMissionStatusFromInt(String value) {
+		switch (value) {
+		case "Average customers per week":
+			return GroceryData.AVG_WEEK_CUSTOMERS;
+		case "Average customers per month":
+			return GroceryData.AVG_MONTH_CUSTOMERS;
+		case "Average time per week":
+			return GroceryData.AVG_TIME_WEEK;
+		case "Average time per month":
+			return GroceryData.AVG_TIME_MONTH;
+		}
+		return null;
+	}
+
+	public String getValue() {
+		return value;
+	}
 }
