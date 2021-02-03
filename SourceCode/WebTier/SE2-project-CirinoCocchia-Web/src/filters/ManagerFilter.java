999688714b781faecbf02a859b5d7a5595cb42ab
duiliocirino <10621933@polimi.it>
2021-02-03 15:01:56 +0100

    Added two new filters and refactored some servlets.

---
+++ SourceCode/WebTier/SE2-project-CirinoCocchia-Web/src/filters/ManagerFilter.java
@@ -0,0 +1,65 @@
+package filters;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import src.main.java.model.User;
+import src.main.java.utils.Roles;
+
+/**
+ * Servlet Filter implementation class ManagerFilter
+ */
+@WebFilter("/ManagerFilter")
+public class ManagerFilter implements Filter {
+
+    /**
+     * Default constructor. 
+     */
+    public ManagerFilter() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		
+		//ALLOW ONLY MANAGERS TO DO THIS OPERATION
+		
+		HttpServletRequest req = (HttpServletRequest) request;
+		HttpServletResponse res = (HttpServletResponse) response;
+		HttpSession session = req.getSession();
+		User user = (User) session.getAttribute("user");
+				
+		if(user.getRole() != Roles.MANAGER) {
+			res.sendError(HttpServletResponse.SC_BAD_REQUEST, "You are not allowed to do this operation");
+			return;
+		}
+		// pass the request along the filter chain
+		chain.doFilter(request, response);
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
